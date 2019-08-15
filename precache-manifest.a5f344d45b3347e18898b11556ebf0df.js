199d79a0c146effb425dffabbf1ba2ddbccc8c21
KrzysztofSiek <krzysztof.siek85@gmail.com>
2019-08-15 18:11:14 +0200

    Updates

---
+++ precache-manifest.a5f344d45b3347e18898b11556ebf0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a0f86e01f60f6ca4938d112a9c3900",
+    "url": "/to-do-app/./index.html"
+  },
+  {
+    "revision": "4f659ece06aef931e6b1",
+    "url": "/to-do-app/./static/css/main.c9bbdd32.chunk.css"
+  },
+  {
+    "revision": "460e0e902c5e9cf347a2",
+    "url": "/to-do-app/./static/js/2.4c76d912.chunk.js"
+  },
+  {
+    "revision": "4f659ece06aef931e6b1",
+    "url": "/to-do-app/./static/js/main.b108ade4.chunk.js"
+  },
+  {
+    "revision": "2ac3bdbc7a43c4c578b0",
+    "url": "/to-do-app/./static/js/runtime~main.1a75560d.js"
+  },
+  {
+    "revision": "70ccb8d0ceaf01b307f3d5a43d9de53b",
+    "url": "/to-do-app/./static/media/cloud-formation-clouds-cloudy-247478.70ccb8d0.jpg"
+  }
+]);
